2b8c0f7c8e9980d6c2b239e6ea3410a1bcbe51fc
邓佐波 <dengzuobo@dengzuobodemacbook-pro.local>
2019-01-17 11:07:44 +0800

    第一次提交

--- TestGitAndGitHub/ViewController.swift
+++ TestGitAndGitHub/ViewController.swift
@@ -1,18 +1,12 @@
-//
-//  ViewController.swift
-//  TestGitAndGitHub
-//
-//  Created by 邓佐波 on 2019/1/17.
-//  Copyright © 2019年 dengzuobo. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
